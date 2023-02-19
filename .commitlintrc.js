ec05a7e410c33cbfed3d0e96ac6759171ddc2703
jinyong_qiu <jinyong_qiu@kingdee.com>
2023-02-19 20:04:13 +0800

    feat: init

---
+++ .commitlintrc.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Leon
+ * @Date: 2023-02-19 18:21:59
+ * @LastEditors: 最后编辑
+ * @LastEditTime: 2023-02-19 18:22:37
+ * @description: 文件说明
+ */
+module.exports = {
+	extends: ['@commitlint/config-conventional']
+};
